a36f81ce9add3a1a9cea0334379e625415f1b783
Marcellort <64420964+Marcellort@users.noreply.github.com>
2020-10-21 23:10:41 -0300

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader } from '@material-ui/core';
 export default () => (
     <Card>
         <CardHeader title="Bem vindo Mochileiro" style={{textAlign: 'center'}} />
-        <img src="https://cdn.pixabay.com/photo/2016/10/18/08/13/travel-1749508_960_720.jpg" style={{width:'100%', height:'50%'}}/>
+        <img src="https://cdn.pixabay.com/photo/2017/03/25/16/52/sunset-2173918_960_720.jpg" style={{width:'100%', height:'50%'}}/>
         <h2 style={{textAlign: 'center'}}>Olá compartilhe conosco suas jornadas</h2>
         <h4><span/></h4>
 
